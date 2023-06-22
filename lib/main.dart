ffa3bad3f7ab07059e86039c90992c5b8938b495
timlocott <tsam.cottrell@gmail.com>
2023-06-22 00:17:07 -0600

    color scheme adjustment

--- lib/main.dart
+++ lib/main.dart
@@ -15,23 +15,7 @@ class PhotoApp extends StatelessWidget {
     return MaterialApp(
       title: 'Photo App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 24, 61, 33)),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.green),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Liz Cottrell Photography'),
@@ -131,7 +115,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Home'));
+    return const Center(
+      child: Text('Home'),
+    );
   }
 }
 
